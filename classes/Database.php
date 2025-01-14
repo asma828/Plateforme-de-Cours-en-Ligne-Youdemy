b08713c5f4d5232478e0820144bbfc35b1e227c8
asma828 <asmalachhab66@gmail.com>
2025-01-15 00:17:54 +0100

    add the database connection with singltone

---
+++ classes/Database.php
@@ -0,0 +1,44 @@
+<?php
+namespace Youdemy\Classes;
+class Database{
+    private static $instance=null;
+    private $connection;
+
+    private function __construct(){
+        $host = '127.0.0.1'; 
+        $db   = 'cours'; 
+        $user = 'root';  
+        $pass = '';  
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->connection = new \PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \Exception("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    private function __clone() {}
+
+}
+    
